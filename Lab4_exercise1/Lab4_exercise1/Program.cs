8694192bd5e7e3133280649cf44bff6e26340fa7
Keith Connors <c17507803@mydit.ie>
2018-10-09 10:50:59 +0100

    bla

--- Lab4_exercise1/Lab4_exercise1/Program.cs
+++ Lab4_exercise1/Lab4_exercise1/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace Lab4_exercise1
+﻿using System;
+using System.Xml.Schema;
+
+namespace Lab4_exercise1
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            
+            for (int i = 0; i <= 20; i++)
+            {
+                Console.WriteLine(i * 8);
+            }
         }
     }
 }
