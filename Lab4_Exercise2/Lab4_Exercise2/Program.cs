146c0153a395982e3ef27000a25e7999699fdd40
Keith Connors <c17507803@mydit.ie>
2018-10-09 10:54:51 +0100

    exercise2

---
+++ Lab4_Exercise2/Lab4_Exercise2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lab4_Exercise2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 50; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
