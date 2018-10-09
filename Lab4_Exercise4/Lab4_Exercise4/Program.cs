e2f7638de13bee204d184be28fddb8431f1cd8cc
Keith Connors <c17507803@mydit.ie>
2018-10-09 12:28:37 +0100

    bla

---
+++ Lab4_Exercise4/Lab4_Exercise4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Lab4_Exercise4
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = 0,
+                x = 1,
+                y,
+                number;
+            
+            Console.WriteLine("Enter a number for the sequence");
+            number = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine(n + " "+ x +" ");
+
+            for (int j = 2; j < number; j++)
+            {
+                y = n + x;
+                Console.WriteLine(y + " ");
+                n = x;
+                x = y;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
