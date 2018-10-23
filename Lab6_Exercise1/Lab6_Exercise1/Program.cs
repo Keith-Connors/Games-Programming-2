474e05c697143ab5a3d9cdd2705c2631c3f093b7
Keith Connors <c17507803@mydit.ie>
2018-10-23 13:55:13 +0100

    Lab exercises.

---
+++ Lab6_Exercise1/Lab6_Exercise1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6_Exercise1
+{
+    class Program
+    { 
+        public static void Main(string[] args)  
+        {
+            int valueX = 10;
+            int valueY = 20;
+            int myResult = multiplyFunction(valueX, valueY);
+                
+            Console.WriteLine("My function result is: "+ myResult);
+            Console.ReadKey();
+        }
+
+        static int multiplyFunction(int a, int b)
+        {
+            var mult = a * b;
+
+            return mult;
+        }
+    }
+}
---
+++ Lab6_Exercise2/Lab6_Exercise2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6_Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valueX, valueY, valueZ;
+        // try parse, int to string, get input
+
+        }
+    }
+}
